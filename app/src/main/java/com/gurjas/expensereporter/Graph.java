7aa1d459adcc682e0b9532e9ae4a2bbb622d11e5
Gurjas604 <gurjas.rai@gmail.com>
2019-08-13 20:19:16 -0700

    Chnages made to view graph
    Still unable to Fix the graph not showing up issue

--- app/src/main/java/com/gurjas/expensereporter/Graph.java
+++ app/src/main/java/com/gurjas/expensereporter/Graph.java
@@ -3,6 +3,11 @@ package com.gurjas.expensereporter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.JavascriptInterface;
 
 public class Graph extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class Graph extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings mwebSettings = myWebView.getSettings();
+        mwebSettings.setJavaScriptEnabled(true);
+        myWebView.setWebChromeClient(new WebChromeClient());
+
+        myWebView.loadUrl("file:///android_asset/index.html");
     }
 }
